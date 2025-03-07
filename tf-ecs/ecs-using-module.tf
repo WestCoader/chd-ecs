0cc8b81d5b9a40de9808ad43c79ee7024f1570b6
WestCoader <WestCoader@outlook.com>
2025-03-06 21:37:12 -0800

    further cleanup and simplifying

--- tf-ecs/ecs-using-module.tf
+++ tf-ecs/ecs-using-module.tf
@@ -1,10 +1,3 @@
-# module "chd" {
-#   source             = "USSBA/easy-fargate-service/aws"
-#   # version            = "~> 4.0"
-#   family             = "my-ez-fargate-service"
-#   container_image    = "nginx:latest"
-# }
-
 resource "aws_ecs_cluster" "chd-ecs" {
   name = "${var.project_name}-cluster"
 
@@ -16,9 +9,7 @@ resource "aws_ecs_cluster" "chd-ecs" {
 
 module "chd-fargate" {
   source             = "USSBA/easy-fargate-service/aws"
-  # version            = "~> 4.0"
   family             = "chd-app"
-  # container_image    = "${aws_ecr_repository.chd-app.repository_url}:latest"
 
   container_definitions = [
     {
@@ -41,27 +32,6 @@ module "chd-fargate" {
   scaling_metric     = "cpu"
   scaling_threshold  = 75
   vpc_id             = "vpc-02b3ad8f2b237c9ea"
-  # private_subnet_ids = ["subnet-080c24f03b5ff312b", "subnet-0d8a3196efe2335f0", "subnet-0d74249c2263e7c60"]
-  # public_subnet_ids  = ["subnet-080c24f03b5ff312b", "subnet-0d8a3196efe2335f0", "subnet-0d74249c2263e7c60"]
-  # certificate_arn    = "arn:aws:acm:us-east-1:123456789012:certificate/12345678-90ab-cdef-1234-567890abcdef"
-  # hosted_zone_id     = "x"
-  # service_fqdn       = "chd.westcoader.com"
   use_cloudfront     = false
-#   cloudfront_header = {
-#     key   = "x-header-name"
-#     value = "12345678-90ab-cdef-1234-567890abcdef"
-#   }
-#   container_environment = [
-#     {
-#       name  = "FOO"
-#       value = "bar"
-#     }
-#   ]
-#   container_secrets = [
-#     {
-#       name      = "FOO_SECRET"
-#       valueFrom = "arn:aws:ssm:${local.region}:${local.account_id}:parameter/foo_secret"
-#     }
-#   ]
   depends_on = [ aws_ecs_cluster.chd-ecs ]
 }
